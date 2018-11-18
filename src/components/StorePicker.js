d43be4e21cc18e7690af7c0390431867f50c4bb0
Matt Freitas <mattfreitas415@gmail.com>
2018-11-18 12:48:15 -0800

    first commit

---
+++ src/components/StorePicker.js
@@ -0,0 +1,17 @@
+import React from 'react'; 
+
+class StorePicker extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <p>Fish!</p>
+        <form className="store-selector">
+          { /* comment in JSX */ }
+          <h2>Please Enter A Store</h2>
+        </form>
+      </React.Fragment>
+    )
+  }
+}
+
+export default StorePicker; 
