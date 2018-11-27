45ece1fcc5152847f6d5749b834b021e0ec56196
Matt Freitas <mattfreitas415@gmail.com>
2018-11-27 14:34:13 -0800

    made state visible in the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Header from './Header';
-import Order from "./Order";
+import Order from './Order';
 import Inventory from "./Inventory"; 
+import sampleFishes from "../sample-fishes";
+import Fish from './Fish';
 
 class App extends React.Component {
   state = {
     fishes: {},
     order: {}
   };
+
   addFish = fish => {
     // how to update state
     // 1. take a copy of existing state using setState api
@@ -20,14 +23,27 @@ class App extends React.Component {
     // 3. set the new fishes object to state
     this.setState({ fishes });
   };
+
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
+
   render() {
     return(
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market"/>
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish key={key} details={this.state.fishes[key]} />
+            ))}
+          </ul>
         </div>
         <Order />
-        <Inventory addFish = {this.addFish} />
+        <Inventory 
+          addFish = {this.addFish} 
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     )
   }
