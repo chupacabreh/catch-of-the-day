f10ce830aef2c76b9f41a990e8027f036189fd25
Matt Freitas <mattfreitas415@gmail.com>
2018-11-28 21:25:48 -0800

    added localstorage

--- src/components/Order.js
+++ src/components/Order.js
@@ -5,7 +5,10 @@ class Order extends React.Component {
   renderOrder = (key) => {
     const fish = this.props.fishes[key];
     const count = this.props.order[key];
-    const isAvaialble = fish.status === 'available';
+    const isAvaialble = fish && fish.status === 'available';
+    //make sure the fish is loaded before we continue
+    if(!fish) return null;
+
     if(!isAvaialble) {
       return <li key={key}>Sorry {fish ? fish.name : 'fish'} is no longer available</li>
     }
